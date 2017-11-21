ef8a3ae5fb88631d9369365f73113bbe86b39419
loksly <loksly@gmail.com>
2017-11-21 15:02:40 +0100

    Added sprites load&save functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 	const path = require('path'),
 		fs = require('fs'),
 		express = require('express'),
+		bodyParser = require('body-parser'),
 		fileUpload = require('express-fileupload'),
 		PythonShell = require('python-shell'),
 		archiver = require('archiver'),
@@ -14,6 +15,12 @@
 	if (typeof config.utility !== 'string'){
 		throw new Error('Utility not properly configured on config.json file');
 	}
+
+	const staticResources = {
+		'rastercoords.js': 'node_modules/leaflet-rastercoords/rastercoords.js',
+		'leaflet.js': 'node_modules/leaflet/dist/leaflet.js',
+		'leaflet.css': 'node_modules/leaflet/dist/leaflet.css'
+	};
 
 	app
 		.get('/api/config', function(req, res){
@@ -26,8 +33,9 @@
 			}
 
 			const file = req.files.map;
-			const now = new Date();
-			const directory = path.join(config.tmpdir, String(now.getTime())),
+			const now = new Date(),
+				dir = String(now.getTime()),
+				directory = path.join(config.tmpdir, dir),
 				filename = path.join(directory, 'original');
 
 			fs.mkdirSync(directory);
@@ -53,12 +61,48 @@
 						if (err4){
 							return res.status(500).json(err4);
 						}
+
 						fs.writeFileSync(path.join(directory, 'tilemapresource.json'), JSON.stringify(result), 'utf8');
+						fs.writeFileSync(path.join(directory, 'sprites.json'), JSON.stringify([]), 'utf8');
+
+						res.json({'id': dir});
+					});
+				});
+			});
+		})
+		.use('/api/map/:id/sprites.json', bodyParser.json()).post('/api/map/:id/sprites.json', function(req, res){
+			const directory = path.join(config.tmpdir, String(parseInt(req.params.id, 10)));
+			const body = typeof req.body === 'string' ? req.body : JSON.stringify(req.body);
+			fs.writeFileSync(path.join(directory, 'sprites.json'), body, 'utf8');
+
+			res.json(body);
+		})
+		.get('/api/map/:id/**', function(req, res){
+			const id = parseInt(req.params.id, 10),
+				directory = path.join(config.tmpdir, String(id)),
+				prefix = '/api/map/' + id + '/';
+
+			if (parseInt(req.params.id, 10) > 0){
+				const filename = req.originalUrl.replace(prefix, '');
+
+				res.sendFile(filename, {root: directory});
+			} else {
+				res.status(404).send('Not found');
+			}
+		})
+		.get('/api/download/:id', function(req, res){
+			if (parseInt(req.params.id, 10) > 0){
+				const directory = path.join(config.tmpdir, String(parseInt(req.params.id, 10)));
+				fs.stat(directory, function(err, stats){
+					if (err){
+						res.status(404).send('Not found');
+					}
+
+					if (stats.isDirectory()){
 						res.writeHead(200, {
 							'Content-Type': 'application/zip',
 							'Content-disposition': 'attachment; filename=map.zip'
 						});
-
 						const zip = archiver('zip', {'zlib': {'level': 9}});
 						zip.pipe(res);
 						zip.file(path.join(__dirname, 'node_modules/leaflet-rastercoords/rastercoords.js'), {name: 'rastercoords.js'});
@@ -67,11 +111,22 @@
 						zip.file(path.join(__dirname, 'public/template/index.html'), {name: 'index.html'});
 						zip.directory(directory, 'map');
 						zip.finalize();
-					});
+					} else {
+						res.status(404).send('Not found');
+					}
 				});
-			});
-		})
-		.use(express.static(path.join(__dirname, 'public')))
+			} else {
+				res.status(404).send('Not found');
+			}
+		});
+
+	Object.keys(staticResources).forEach(function(name){
+		app.get('/' + name, function(req, res){
+			res.sendFile(staticResources[name], {root: __dirname});
+		});
+	});
+
+	app.use(express.static(path.join(__dirname, 'public')))
 		.use(function(req, res, next) {
 			if (req.originalUrl.endsWith('.png')){
 				return res.redirect('/blank.png');
--- public/script.js
+++ public/script.js
@@ -1,13 +1,203 @@
-/* global angular */
-(function(angular){
+/* global angular, window */
+(function(angular, window){
 	'use strict';
 
-	angular.module('maprt', ['ngResource', 'angularFileUpload']).factory('Config', ['$resource', function($resource){
+	angular.module('maprt', ['ngResource', 'ngFileUpload'
+	]).factory('Config', ['$resource', function($resource){
 		return $resource('/api/config');
+	}]).factory('TileMapResource', ['$resource', function($resource){
+		return $resource('/api/map/:id/tilemapresource.json', {id: '@id'});
+	}]).factory('Sprites', ['$resource', function($resource){
+		return $resource('/api/map/:id/sprites.json', {id: '@id'});
 	}]).run(['$rootScope', 'Config', function($rootScope, Config){
 		$rootScope.config = Config.get();
-	}]).controller('Dashboard', ['$scope', 'FileUploader', function($scope, FileUploader){
-		$scope.uploader = new FileUploader({url: '/api/upload'});
+	}]).controller('Dashboard', ['$scope', '$log', '$window', '$q', 'Upload', 'TileMapResource', 'Sprites', function($scope, $log, $window, $q, Upload, TileMapResource, Sprites){
+
+		$scope.sections = [{id: 'Start'}, {id: 'Sprites'}];
+		$scope.selectedSection = 'Start';
+		$scope.setSection = function(section){
+			$scope.selectedSection = section.id;
+		};
+
+		$scope.submit = function($event) {
+			$event.preventDefault();
+			if ($scope.form.file.$valid && $scope.file) {
+				$scope.upload($scope.file);
+			}
+		};
+
+		$scope.sprites = [];
+
+		var impar = false,
+			coords = [],
+			map,
+			rc;
+
+		$scope.numberofsides = 4;
+		$scope.spritesLoader = '[]';
+		$scope.layers = [];
+
+		function noop(){}
+
+		$scope.addPoint = noop;
+
+		$scope.sync = function(){
+			$scope.spritesLoader = angular.toJson($scope.sprites);
+		};
+
+		$scope.addSprite = function(s){
+			$scope.sprites.push(s);
+			$scope.spritesLoader = angular.toJson($scope.sprites);
+		};
+
+		$scope.loadSprites = function(){
+			$scope.layers.forEach(function(layer){
+				map.removeLayer(layer);
+			});
+			$scope.layers = [];
+			$scope.sprites = angular.fromJson($scope.spritesLoader);
+			$scope.sprites.forEach($scope.addToSpriteMap);
+		};
+
+		$scope.addLayer = function(layer){
+			$scope.layers.push(layer);
+			map.addLayer(layer);
+		};
+
+		$scope.addToSpriteMap = function(sprite){
+			var points = sprite.points.map(function (point) {
+				return rc.unproject([point.x, point.y]);
+			});
+			var layer = points.length === 2 ? window.L.rectangle([points]) : window.L.polygon([points]);
+			$scope.addLayer(layer);
+		};
+
+		function addRectangle(rectangle){
+			var sprite = {id: '', type: 'rectangle', points: rectangle};
+			$scope.addSprite(sprite);
+			$scope.addToSpriteMap(sprite);
+		}
+
+		function addPolygon(polygon){
+			var sprite = {id: '', type: 'polygon', points: polygon};
+			$scope.addSprite({id: '', type: 'polygon', points: polygon});
+			$scope.addToSpriteMap(sprite);
+		}
+
+		function addPolygonPoint(latlng){
+			coords.push(latlng);
+			if (coords.length === parseInt($scope.numberofsides, 10)){
+				addPolygon(coords);
+				coords = [];
+			}
+		}
+
+		function addRectanglePoint(latlng){
+			coords.push(latlng);
+			if (coords.length === 2){
+				addRectangle(coords);
+				coords = [];
+			}
+		}
+
+		function onMapClick(e){
+			if (!$scope.editor){
+				return;
+			}
+
+			var point = rc.project(e.latlng);
+			point.x = Math.ceil(point.x);
+			point.y = Math.ceil(point.y);
+			$scope.addPoint(point);
+		}
+
+		$scope.addRectangle = function(){
+			if ($scope.editor === 'rectangle'){
+				$scope.editor = false;
+				$scope.addPoint = noop;
+			} else {
+				$scope.editor = 'rectangle';
+				$scope.addPoint = addRectanglePoint;
+			}
+			coords = [];
+		};
+
+		$scope.addPolygon = function(){
+			if ($scope.editor === 'polygon'){
+				$scope.editor = false;
+				$scope.addPoint = noop;
+			} else {
+				$scope.editor = 'polygon';
+				$scope.addPoint = addPolygonPoint;
+			}
+			coords = [];
+		};
+
+		$scope.mapInit = function(id, mapconfiguration){
+			var minZoom = mapconfiguration.minZoom;
+			var maxZoom = mapconfiguration.maxZoom;
+			var img = [mapconfiguration.width, mapconfiguration.height];
+
+			// create the map
+			map = window.L.map('map', {
+				minZoom: minZoom,
+				maxZoom: maxZoom
+			});
+
+			rc = new window.L.RasterCoords(map, img);
+			map.setView(rc.unproject([mapconfiguration.width / 2, mapconfiguration.height / 2]), maxZoom / 2);
+
+			window.L.tileLayer('/api/map/' + id + '/{z}/{x}/{y}.png', {
+				noWrap: true,
+				attribution: 'Map'
+			}).addTo(map);
+			map.on('click', onMapClick);
+		};
+
+		$scope.loadMap = function(id){
+			var loads = [Sprites.get({id: id}), TileMapResource.get({id: id})];
+			$q.all(loads.map(function(o){ return o.$promise; })).then(function(){
+
+				var o = loads[1];
+				$scope.tilemap = o.TileMap;
+				$scope.mapid = id;
+
+				$scope.mapconfiguration = {
+					width: o.TileMap.BoundingBox[0].$.maxx - o.TileMap.BoundingBox[0].$.minx,
+					height: o.TileMap.BoundingBox[0].$.maxy - o.TileMap.BoundingBox[0].$.miny,
+					minZoom: 0,
+					maxZoom: o.TileMap.TileSets[0].TileSet.length - 1
+				};
+				$scope.mapInit(id, $scope.mapconfiguration);
+				$scope.sprites = loads[0].sprites;
+				$scope.sprites.forEach($scope.addToSpriteMap);
+			});
+		};
+
+		$scope.saveSprites = function(){
+			var newSave = new Sprites({id: $scope.mapid, sprites: $scope.sprites});
+			newSave.$save();
+		};
+
+		$scope.download = function(){
+			$window.location = '/api/download/' + $scope.mapid;
+		};
+
+		$scope.upload = function (file) {
+			$log.log(file);
+			Upload.upload({
+				url: '/api/upload',
+				data: {map: file}
+			}).then(function (resp) {
+				var answer = typeof resp.data === 'string' ? JSON.parse(resp.data) : resp.data;
+				$scope.loadMap(answer.id);
+			}, function (resp) {
+				$log.error('Error status:', resp.status);
+			});
+		};
+
+		$scope.loadMap(1511256908943);
+
 	}]);
 
-})(angular);
+})(angular, window);
